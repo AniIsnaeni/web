5b3d35455b3f712478f7716c6053cc7a59966b69
AniIsnaeni <85162868+AniIsnaeni@users.noreply.github.com>
2021-07-20 00:31:43 +0700

    Create dashboard1.php

---
+++ dashboard1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>DTokyo Olympics 2020</title>
+ <link rel="stylesheet" href="style2.css">
+ <link rel="stylesheet" href="style2.css" />
+</head>
+<body>
+ <div class="navbar">
+  <h3 class="admin">Tokyo Olympics 2020</h3>
+ </div>
+
+ <div class="wrapper">
+  <div class="boxx">
+   <div class="box1">
+    <i class="fa fa-home fa-5x" style="color: white;margin:30px 20px"></i>
+    <div class="title"><h2 class="int"><a href='sorting.php'>Data </h2></div>
+   </div>
+   
+   <div class="box3">
+    <i class="fa fa-list fa-5x" style="color: white;margin:30px 20px"></i>
+    <div class="title"><h2 class="inttt"><a href='index2.php'>EDIT</h2></div>
+   </div>
+  </div>
+
+ </div>
+</body>
+</html>
+
+
