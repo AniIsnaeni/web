a14e4f1cf1b1ae1d1b936b940e102fbd60aeda25
AniIsnaeni <85162868+AniIsnaeni@users.noreply.github.com>
2021-07-20 00:39:36 +0700

    Create index.php
    halaman login

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+
+<div class="navbar">
+  <h3 class="admin">Tokyo Olympics 2020</h3>
+ </div>
+
+<body>
+<div id="card"></div>
+    <br>
+    <br>
+<center>
+    <table>
+        <form  method="POST" action="aksi_login.php">
+        <tr> <td colspan="2" align="center">form login</td></tr>
+       
+        <tr><td>User Name</td><td><input type="text" name="username"></td></tr>
+        <tr><td>Password</td><td><input type="password" name="password"></td></tr>
+        <tr><td colspan="2" align="right">
+            <input type="submit" value="LOGIN">
+        </td></tr>
+        </form>
+    </table>
+
+</center>    
+</body>
+</html>
