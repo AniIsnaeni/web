421cd748b8a325f260bbbb65ab53a3577cb63cf6
AniIsnaeni <85162868+AniIsnaeni@users.noreply.github.com>
2021-07-20 00:59:35 +0700

    Create utama.php

---
+++ utama.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+  <meta charset="utf-8" />
+  <title>Belajar HTML5</title>
+</head>
+ 
+<body>
+<div class="topnav">
+  <a class="active" href="#home">input</a>
+  <a href="#news">edit</a>
+  <a href="#contact">VIP</a>
+</div>
+</body>
+</body>
+</html>
